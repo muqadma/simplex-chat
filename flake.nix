9574a3e5d92dd0941f2a56aad2ad90b5ad6c57fe
Faez Shakil <faez.shakil@gmail.com>
2024-07-16 20:17:22 +0500

    a bot for uploading to the provided storage backend

--- flake.nix
+++ flake.nix
@@ -610,12 +610,42 @@
           text = ''
             gawk -f ./scripts/nix/update-sha256.awk cabal.project > ./scripts/nix/sha256map.nix
           '';
-        }; in
+  };
+  hsShell = (drv pkgs).shellFor {
+    # ALL of these arguments are optional.
+
+    # List of packages from the project you want to work on in
+    # the shell (default is all the projects local packages).
+    # packages = ps: with ps; [
+    #   pkga
+    #   pkgb
+    # ];
+
+    # Builds a Hoogle documentation index of all dependencies,
+    # and provides a "hoogle" command to search the index.
+    withHoogle = true;
+
+    # Some common tools can be added with the `tools` argument
+    tools = {
+      cabal = "latest";
+      hlint = "latest"; # Selects the latest version in the hackage.nix snapshot
+      haskell-language-server = "latest";
+    };
+    # See overlays/tools.nix for more details
+
+    # Some you may need to get some other way.
+    # buildInputs = [ pkgs.git ];
+
+    # Prevents cabal from choosing alternate plans, so that
+    # *all* dependencies are provided by Nix.
+    exactDeps = false;
+  }; in
 	pkgs.mkShell {
           buildInputs = [ updateCmd ];
           shellHook = ''
             echo "welcome to the shell!"
           '';
+          inputsFrom = [ hsShell ];
         };
       }
     );
